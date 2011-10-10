1346a44e46a1c3e939c9d391804ae114e7ab2180
Jørn Ølmheim <jorn@olmheim.com>
2011-10-11 01:20:24 +0200

    Added remove_attribute method

--- spec/xml_updater_test.rb
+++ spec/xml_updater_test.rb
@@ -1,13 +1,122 @@
 require "test/unit"
 require "xml_updater"
 class XmlUpdaterTest < Test::Unit::TestCase
-  def test_parse_xml
-    xml_updater = XmlUpdater.new '/home/joe/code/3DPosModel/Heidrun/dataset.sct.xml'
+  def test_parse_xml_file
+    xml_updater = XmlUpdater.new 'spec/test.xml'
+    assert_equal('document', xml_updater.xml_document.name)
+  end
+  
+  def test_parse_xml_string
+    xml_updater = XmlUpdater.new "<?xml version=\"1.0\"?>\n<root><horizons><item><id>1</id></item></horizons></root>"
     assert_equal('document', xml_updater.xml_document.name)
   end
   
   def test_get_element
-    xml_updater = XmlUpdater.new '/home/joe/code/3DPosModel/Heidrun/dataset.sct.xml'
-    assert_equal('Seabed', xml_updater.get_element('//dataset/seabedhorizonid').content)
+    xml_updater = XmlUpdater.new "<?xml version=\"1.0\"?>\n<root><horizons><item><id>1</id></item></horizons></root>"
+    assert_equal('1', xml_updater.get_element('//horizons/item/id').content)
   end
+  
+  def test_update_date_format
+    expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <dates>
+    <item>
+      <date>2010-11-01 00:00:00</date>
+    </item>
+    <item>
+      <date>2010-11-01 00:00:00</date>
+    </item>
+    <item>
+      <date>2010-11-01 00:00:00</date>
+    </item>
+  </dates>
+</root>\n"
+    actual_xml = "<?xml version=\"1.0\"?>
+<root>
+  <dates>
+    <item>
+      <date>2010-11-01</date>
+    </item>
+    <item>
+      <date>2010-11-01</date>
+    </item>
+    <item>
+      <date>2010-11-01</date>
+    </item>
+  </dates>
+</root>"
+    xml_updater = XmlUpdater.new actual_xml
+    xml_updater.update_date_format('//dates/item/date', '%Y-%m-%d %H:%M:%S')
+    assert_equal(expected_xml, xml_updater.xml_document.to_xml)
+  end
+  
+  def test_update_content_with_counter
+    expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <name>Test1</name>
+    </item>
+    <item>
+      <name>Test2</name>
+    </item>
+    <item>
+      <name>Test3</name>
+    </item>
+  </names>
+</root>\n"
+    actual_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <name>Un</name>
+    </item>
+    <item>
+      <name>Deux</name>
+    </item>
+    <item>
+      <name>Trois</name>
+    </item>
+  </names>
+</root>"
+    xml_updater = XmlUpdater.new actual_xml
+    xml_updater.update_content_with_counter('//names/item/name', 'Test')
+    assert_equal(expected_xml, xml_updater.xml_document.to_xml)
+  end
+  
+  def test_remove_attribute
+    expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <name>Test1</name>
+    </item>
+    <item>
+      <name>Test2</name>
+    </item>
+    <item>
+      <name>Test3</name>
+    </item>
+  </names>
+</root>\n"
+    actual_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <name id=\"1\">Test1</name>
+    </item>
+    <item>
+      <name id=\"2\">Test2</name>
+    </item>
+    <item>
+      <name id=\"3\">Test3</name>
+    </item>
+  </names>
+</root>"
+    xml_updater = XmlUpdater.new actual_xml
+    xml_updater.remove_attribute('//names/item/name', 'id')
+    assert_equal(expected_xml, xml_updater.xml_document.to_xml)
+  end
+  
+  
 end
--- src/xml_updater.rb
+++ src/xml_updater.rb
@@ -1,19 +1,19 @@
 require 'rubygems'
 require 'nokogiri'
+require 'fileutils'
 
 class XmlUpdater
   attr_accessor :xml_document
   
-  def initialize(path_to_xml_file)
-    @xml_file = path_to_xml_file
-    parse_xml @xml_file
+  def initialize(string_or_xml_file)
+    parse_xml string_or_xml_file
   end
 
   def get_element(element_name)
     return @xml_document.xpath(element_name).first
   end
   
-  def updateDateContent(xpath, new_format)
+  def update_date_format(xpath, new_format)
     elements = @xml_document.xpath(xpath)
     elements.each do |element|
       element_date = Date.parse element.content
@@ -21,17 +21,39 @@ class XmlUpdater
     end
   end
   
+  def update_content_with_counter(xpath, new_content)
+    elements = @xml_document.xpath(xpath)
+    counter = 0
+    elements.each do |element|
+      counter += 1
+      element.content = new_content + counter.to_s
+    end
+  end
+  
+  def remove_attribute(xpath, attribute_name)
+    elements = @xml_document.xpath(xpath)
+    elements.each do |element|
+      element.delete(attribute_name)
+    end
+  end
+  
   def write_xml()
+    FileUtils.cp(string_or_xml_file, string_or_xml_file + '.bak')
     File.open(@xml_file, 'w') do |f|
       f.write(@xml_document.to_xml)
     end
+    rescue Exception
+      puts "You shouldn't call write_xml() when you haven't passed a filename as argument."
   end
 
 private 
-  def parse_xml(path_to_xml_document) 
-    File.open(path_to_xml_document) do |f|
+  def parse_xml(string_or_xml_file)
+    File.open(string_or_xml_file) do |f|
+      @xml_file = string_or_xml_file
       @xml_document = Nokogiri::XML(f)
     end
+    rescue Exception 
+      @xml_document = Nokogiri::XML(string_or_xml_file)  
   end
 
 end
