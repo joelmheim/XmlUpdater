c6b3414ee35bb21ba30481a3f3180049c80cb8f1
Jørn Ølmheim <joe@statoil.com>
2016-11-01 00:33:01 +0100

    Moved all tests to rspec

--- spec/xml_updater_spec.rb
+++ spec/xml_updater_spec.rb
@@ -124,321 +124,328 @@ RSpec.describe XmlUpdater, '#parse' do
     end
   end
 
-  #end
+  context "with element counter" do
+    it "updates content with counter" do
+      expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <name>Test1</name>
+    </item>
+    <item>
+      <name>Test2</name>
+    </item>
+    <item>
+      <name>Test3</name>
+    </item>
+  </names>
+</root>\n"
+      actual_xml = "<?xml version=\"1.0\"?>
+        <root>
+          <names>
+            <item>
+              <name>Un</name>
+            </item>
+            <item>
+              <name>Deux</name>
+            </item>
+            <item>
+              <name>Trois</name>
+            </item>
+          </names>
+        </root>"
+      xml_updater = XmlUpdater.new actual_xml
+      xml_updater.update_content_with_counter('//names/item/name', 'Test')
+      expect(xml_updater.to_xml).to eq expected_xml
+    end
+  end
 
+  context "with remove attribute" do
+    it "removes attribute" do
+      expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <name>Test1</name>
+    </item>
+    <item>
+      <name>Test2</name>
+    </item>
+    <item>
+      <name>Test3</name>
+    </item>
+  </names>
+</root>\n"
+      actual_xml = "<?xml version=\"1.0\"?>
+        <root>
+          <names>
+            <item>
+              <name id=\"1\">Test1</name>
+            </item>
+            <item>
+              <name id=\"2\">Test2</name>
+            </item>
+            <item>
+              <name id=\"3\">Test3</name>
+            </item>
+          </names>
+        </root>"
+      xml_updater = XmlUpdater.new actual_xml
+      xml_updater.remove_attribute('//names/item/name', 'id')
+      expect(xml_updater.to_xml).to eq expected_xml
+    end
+  end
+
+  context "with xml content" do
+    it "updates content" do
+      expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <value>999</value>
+    </item>
+    <item>
+      <value>999</value>
+    </item>
+    <item>
+      <value>999</value>
+    </item>
+  </names>
+</root>\n"
+      actual_xml = "<?xml version=\"1.0\"?>
+        <root>
+          <names>
+            <item>
+              <value>123</value>
+            </item>
+            <item>
+              <value>213</value>
+            </item>
+            <item>
+              <value>312</value>
+            </item>
+          </names>
+        </root>"
+      xml_updater = XmlUpdater.new actual_xml
+      xml_updater.update_content('//names/item/value', '999')
+      expect(xml_updater.to_xml).to eq expected_xml
+    end
+  end
+
+  context "with xml document" do
+    it "adds element" do
+      expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <name>Test1</name>
+      <value>999</value>
+    </item>
+    <item>
+      <name>Test2</name>
+      <value>999</value>
+    </item>
+    <item>
+      <name>Test3</name>
+      <value>999</value>
+    </item>
+  </names>
+</root>\n"
+      actual_xml = "<?xml version=\"1.0\"?>
+        <root>
+          <names>
+            <item>
+              <name>Test1</name>
+            </item>
+            <item>
+              <name>Test2</name>
+            </item>
+            <item>
+              <name>Test3</name>
+            </item>
+          </names>
+        </root>"
+      xml_updater = XmlUpdater.new actual_xml
+      xml_updater.add_child_element('//names/item', 'value', '999')
+      expect(xml_updater.to_xml).to eq expected_xml
+    end
+
+    it "adds element with type" do
+      expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <name>Test1</name>
+      <value type=\"double\">999</value>
+    </item>
+    <item>
+      <name>Test2</name>
+      <value type=\"double\">999</value>
+    </item>
+    <item>
+      <name>Test3</name>
+      <value type=\"double\">999</value>
+    </item>
+  </names>
+</root>\n"
+      actual_xml = "<?xml version=\"1.0\"?>
+        <root>
+          <names>
+            <item>
+              <name>Test1</name>
+            </item>
+            <item>
+              <name>Test2</name>
+            </item>
+            <item>
+              <name>Test3</name>
+            </item>
+          </names>
+        </root>"
+      xml_updater = XmlUpdater.new actual_xml
+      xml_updater.add_child_element('//names/item', 'value', '999', 'double')
+      expect(xml_updater.to_xml).to eq expected_xml
+    end
+
+    it "adds element when existing" do
+      expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <name>Test1</name>
+      <value>999</value>
+    </item>
+    <item>
+      <name>Test2</name>
+      <value>999</value>
+    </item>
+    <item>
+      <name>Test3</name>
+      <value>999</value>
+    </item>
+  </names>
+</root>\n"
+      actual_xml = "<?xml version=\"1.0\"?>
+        <root>
+          <names>
+            <item>
+              <name>Test1</name>
+              <value>999</value>
+            </item>
+            <item>
+              <name>Test2</name>
+              <value>999</value>
+            </item>
+            <item>
+              <name>Test3</name>
+              <value>999</value>
+            </item>
+          </names>
+        </root>"
+      xml_updater = XmlUpdater.new actual_xml
+      xml_updater.add_child_element('//names/item', 'value', '999')
+      expect(xml_updater.to_xml).to eq expected_xml
+    end
+
+    it "adds element to root with child" do
+      expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <test/>
+  <foo>
+    <bar>1</bar>
+  </foo>
+</root>\n"
+      actual_xml = "<?xml version=\"1.0\"?>
+        <root>
+          <test/>
+        </root>"
+      xml_updater = XmlUpdater.new actual_xml
+      xml_updater.add_element_to_root('foo')
+      xml_updater.add_child_element('//foo', 'bar', 1)
+      expect(xml_updater.to_xml).to eq expected_xml
+    end
+
+    it "adds element to root where exists" do
+      expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <test/>
+  <foo>
+    <bar>1</bar>
+  </foo>
+</root>\n"
+      actual_xml = "<?xml version=\"1.0\"?>
+        <root>
+          <test/>
+          <foo>
+            <bar>1</bar>
+          </foo>
+        </root>"
+      xml_updater = XmlUpdater.new actual_xml
+      xml_updater.add_element_to_root('foo')
+      xml_updater.add_child_element('//foo', 'bar', 1)
+      expect(xml_updater.to_xml).to eq expected_xml
+    end
+
+    it "adds element to root where another root element has same child" do
+      expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <test>
+    <foo>0</foo>
+  </test>
+  <foo>
+    <bar>1</bar>
+  </foo>
+</root>\n"
+      actual_xml = "<?xml version=\"1.0\"?>
+        <root>
+          <test>
+            <foo>0</foo>
+          </test>
+        </root>"
+      xml_updater = XmlUpdater.new actual_xml
+      xml_updater.add_element_to_root('foo')
+      xml_updater.add_child_element('/root/foo', 'bar', 1)
+      expect(xml_updater.to_xml).to eq expected_xml
+    end
+  end
+
+  context "with remove element" do
+    it "removes element" do
+      expected_xml = "<?xml version=\"1.0\"?>
+<root>
+  <names>
+    <item>
+      <value>999</value>
+    </item>
+    <item>
+      <value>999</value>
+    </item>
+    <item>
+      <value>999</value>
+    </item>
+  </names>
+</root>\n"
+      actual_xml = "<?xml version=\"1.0\"?>
+        <root>
+          <names>
+            <item>
+              <name>Test1</name>
+              <value>999</value>
+            </item>
+            <item>
+              <name>Test2</name>
+              <value>999</value>
+            </item>
+            <item>
+              <name>Test3</name>
+              <value>999</value>
+            </item>
+          </names>
+        </root>"
+      xml_updater = XmlUpdater.new actual_xml
+      xml_updater.remove_element('//names/item/name')
+      expect(xml_updater.to_xml).to eq expected_xml
+    end
+  end
 
 end
-
-
-
-#
-#   def test_update_content_with_counter
-#     expected_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name>Test1</name>
-#     </item>
-#     <item>
-#       <name>Test2</name>
-#     </item>
-#     <item>
-#       <name>Test3</name>
-#     </item>
-#   </names>
-# </root>\n"
-#     actual_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name>Un</name>
-#     </item>
-#     <item>
-#       <name>Deux</name>
-#     </item>
-#     <item>
-#       <name>Trois</name>
-#     </item>
-#   </names>
-# </root>"
-#     xml_updater = XmlUpdater.new actual_xml
-#     xml_updater.update_content_with_counter('//names/item/name', 'Test')
-#     assert_equal(expected_xml, xml_updater.to_xml)
-#   end
-#
-#   def test_remove_attribute
-#     expected_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name>Test1</name>
-#     </item>
-#     <item>
-#       <name>Test2</name>
-#     </item>
-#     <item>
-#       <name>Test3</name>
-#     </item>
-#   </names>
-# </root>\n"
-#     actual_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name id=\"1\">Test1</name>
-#     </item>
-#     <item>
-#       <name id=\"2\">Test2</name>
-#     </item>
-#     <item>
-#       <name id=\"3\">Test3</name>
-#     </item>
-#   </names>
-# </root>"
-#     xml_updater = XmlUpdater.new actual_xml
-#     xml_updater.remove_attribute('//names/item/name', 'id')
-#     assert_equal(expected_xml, xml_updater.to_xml)
-#   end
-#
-#   def test_update_content
-#     expected_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <value>999</value>
-#     </item>
-#   </names>
-# </root>\n"
-#     actual_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <value>123</value>
-#     </item>
-#     <item>
-#       <value>213</value>
-#     </item>
-#     <item>
-#       <value>312</value>
-#     </item>
-#   </names>
-# </root>"
-#     xml_updater = XmlUpdater.new actual_xml
-#     xml_updater.update_content('//names/item/value', '999')
-#     assert_equal(expected_xml, xml_updater.to_xml)
-#   end
-#
-#   def test_add_element
-#     expected_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name>Test1</name>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <name>Test2</name>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <name>Test3</name>
-#       <value>999</value>
-#     </item>
-#   </names>
-# </root>\n"
-#     actual_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name>Test1</name>
-#     </item>
-#     <item>
-#       <name>Test2</name>
-#     </item>
-#     <item>
-#       <name>Test3</name>
-#     </item>
-#   </names>
-# </root>"
-#     xml_updater = XmlUpdater.new actual_xml
-#     xml_updater.add_child_element('//names/item', 'value', '999')
-#     assert_equal(expected_xml, xml_updater.to_xml)
-#   end
-#
-#   def test_add_element_with_type
-#     expected_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name>Test1</name>
-#       <value type=\"double\">999</value>
-#     </item>
-#     <item>
-#       <name>Test2</name>
-#       <value type=\"double\">999</value>
-#     </item>
-#     <item>
-#       <name>Test3</name>
-#       <value type=\"double\">999</value>
-#     </item>
-#   </names>
-# </root>\n"
-#     actual_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name>Test1</name>
-#     </item>
-#     <item>
-#       <name>Test2</name>
-#     </item>
-#     <item>
-#       <name>Test3</name>
-#     </item>
-#   </names>
-# </root>"
-#     xml_updater = XmlUpdater.new actual_xml
-#     xml_updater.add_child_element('//names/item', 'value', '999', 'double')
-#     assert_equal(expected_xml, xml_updater.to_xml)
-#   end
-#
-#   def test_add_element_when_existing
-#     expected_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name>Test1</name>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <name>Test2</name>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <name>Test3</name>
-#       <value>999</value>
-#     </item>
-#   </names>
-# </root>\n"
-#     actual_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name>Test1</name>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <name>Test2</name>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <name>Test3</name>
-#       <value>999</value>
-#     </item>
-#   </names>
-# </root>"
-#     xml_updater = XmlUpdater.new actual_xml
-#     xml_updater.add_child_element('//names/item', 'value', '999')
-#     assert_equal(expected_xml, xml_updater.to_xml)
-#   end
-#
-#   def test_add_element_to_root_with_child
-#     expected_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <test/>
-#   <foo><bar>1</bar></foo>
-# </root>\n"
-#     actual_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <test/>
-# </root>"
-#     xml_updater = XmlUpdater.new actual_xml
-#     xml_updater.add_element_to_root('foo')
-#     xml_updater.add_child_element('//foo', 'bar', 1)
-#     assert_equal(expected_xml, xml_updater.to_xml)
-#   end
-#
-#   def test_add_element_to_root_where_exists
-#     expected_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <test/>
-#   <foo>
-#     <bar>1</bar>
-#   </foo>
-# </root>\n"
-#     actual_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <test/>
-#   <foo>
-#     <bar>1</bar>
-#   </foo>
-# </root>"
-#     xml_updater = XmlUpdater.new actual_xml
-#     xml_updater.add_element_to_root('foo')
-#     xml_updater.add_child_element('//foo', 'bar', 1)
-#     assert_equal(expected_xml, xml_updater.to_xml)
-#   end
-#
-#   def test_add_element_to_root_where_another_root_element_has_same_child
-#     expected_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <test>
-#     <foo>0</foo>
-#   </test>
-#   <foo><bar>1</bar></foo>
-# </root>\n"
-#     actual_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <test>
-#     <foo>0</foo>
-#   </test>
-# </root>"
-#     xml_updater = XmlUpdater.new actual_xml
-#     xml_updater.add_element_to_root('foo')
-#     xml_updater.add_child_element('/root/foo', 'bar', 1)
-#     assert_equal(expected_xml, xml_updater.to_xml)
-#   end
-#
-#   def test_remove_element
-#     expected_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <value>999</value>
-#     </item>
-#   </names>
-# </root>\n"
-#     actual_xml = "<?xml version=\"1.0\"?>
-# <root>
-#   <names>
-#     <item>
-#       <name>Test1</name>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <name>Test2</name>
-#       <value>999</value>
-#     </item>
-#     <item>
-#       <name>Test3</name>
-#       <value>999</value>
-#     </item>
-#   </names>
-# </root>"
-#     xml_updater = XmlUpdater.new actual_xml
-#     xml_updater.remove_element('//names/item/name')
-#     assert_equal(expected_xml, xml_updater.to_xml)
-#   end
-# end
